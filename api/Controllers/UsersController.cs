e70d6e01fffa93dfb487efc4472c8c9607cbc29f
sathvikb <sathvik4@gmail.com>
2020-12-28 16:57:26 -0500

    lol

--- api/Controllers/UsersController.cs
+++ api/Controllers/UsersController.cs
@@ -12,6 +12,7 @@ namespace api.Controllers
     [Route("api/[controller]")]
     public class UsersController : ControllerBase
     {
+        int a ;
         private readonly DataContext _context;
         public UsersController(DataContext context)
         {
